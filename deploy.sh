10b89220db6364c951b420a38e79fdbe17832929
huweihuang <huweihuang@foxmail.com>
2018-10-10 20:49:50 +0800

    MOD: Add kubelet code analysis

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,33 @@ set -x
 set -e
 
 MESSAGE=$1
-MASTER_DIR="/Users/meitu/hexo/kubernetes-notes"
-GHPAGE_DIR="/Users/meitu/hexo/k8s-gh-pages"
+
+PROGRAM="kubernetes"
+GITBOOK_DIR="/Users/meitu/hexo/gitbook"
+MASTER_DIR="${GITBOOK_DIR}/${PROGRAM}-notes"
+GHPAGE_DIR="${GITBOOK_DIR}/${PROGRAM}-gh-pages"
+CODING_DIR="${GITBOOK_DIR}/${PROGRAM}-coding-pages"
 
 # build
 gitbook build
 
 # clean GHPAGE_DIR
 rm -fr ${GHPAGE_DIR}/*
+rm -fr ${CODING_DIR}/*
 
 # copy _book to GHPAGE_DIR
 cp -fr ${MASTER_DIR}/_book/* ${GHPAGE_DIR}
 cp -fr ${MASTER_DIR}/README.md ${GHPAGE_DIR}
+cp -fr ${MASTER_DIR}/_book/* ${CODING_DIR}
+cp -fr ${MASTER_DIR}/README.md ${CODING_DIR}
 
 # git commit
 cd ${GHPAGE_DIR}
 git add --all
 git commit -m "${MESSAGE}"
 git push origin gh-pages
+
+cd ${CODING_DIR}
+git add --all
+git commit -m "${MESSAGE}"
+git push origin coding-pages
