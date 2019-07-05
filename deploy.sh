62a3ca91532924f1757145bc09c0544a9e06911d
huweihuang <huweihuang@foxmail.com>
2019-07-05 20:44:11 +0800

    FIX: Add sidebar links

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,23 @@ set -e
 MESSAGE=$1
 
 PROGRAM="kubernetes"
-GITBOOK_DIR="/Users/meitu/hexo/gitbook"
+GITBOOK_DIR="/Users/weihuanghu/hexo/gitbook"
 MASTER_DIR="${GITBOOK_DIR}/${PROGRAM}-notes"
 GHPAGE_DIR="${GITBOOK_DIR}/${PROGRAM}-gh-pages"
-CODING_DIR="${GITBOOK_DIR}/${PROGRAM}-coding-pages"
+# CODING_DIR="${GITBOOK_DIR}/${PROGRAM}-coding-pages"
 
 # build
 gitbook build
 
 # clean GHPAGE_DIR
 rm -fr ${GHPAGE_DIR}/*
-rm -fr ${CODING_DIR}/*
+# rm -fr ${CODING_DIR}/*
 
 # copy _book to GHPAGE_DIR
 cp -fr ${MASTER_DIR}/_book/* ${GHPAGE_DIR}
 cp -fr ${MASTER_DIR}/README.md ${GHPAGE_DIR}
-cp -fr ${MASTER_DIR}/_book/* ${CODING_DIR}
-cp -fr ${MASTER_DIR}/README.md ${CODING_DIR}
+# cp -fr ${MASTER_DIR}/_book/* ${CODING_DIR}
+# cp -fr ${MASTER_DIR}/README.md ${CODING_DIR}
 
 # git commit
 cd ${GHPAGE_DIR}
@@ -30,7 +30,7 @@ git add --all
 git commit -m "${MESSAGE}"
 git push origin gh-pages
 
-cd ${CODING_DIR}
-git add --all
-git commit -m "${MESSAGE}"
-git push origin coding-pages
+# cd ${CODING_DIR}
+# git add --all
+# git commit -m "${MESSAGE}"
+# git push origin coding-pages
